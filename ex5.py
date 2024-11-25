d913cfb5034ecd7f764b1e2ceb0eb92b79097d41
LuisEduardo <luis.e@ges.inatel.br>
2024-11-24 22:35:43 -0300

    feat: ex5

--- ex5.py
+++ ex5.py
@@ -3,25 +3,27 @@ import pandas as pd
 
 
 # 1
-# dfEmpresas = pd.read_csv('data/space.csv', delimiter=';')
+dfEmpresas = pd.read_csv('data/space.csv', delimiter=';')
 
-# dfEmpresas = dfEmpresas.drop_duplicates(subset='Company Name')
-# empresas_EUA = dfEmpresas['Location'].str.contains('USA').sum()
-# empresas_CHINA = dfEmpresas['Location'].str.contains('China').sum()
+dfEmpresas = dfEmpresas.drop_duplicates(subset='Company Name')
+empresas_EUA = dfEmpresas['Location'].str.contains('USA').sum()
+empresas_CHINA = dfEmpresas['Location'].str.contains('China').sum()
 
-# print('Empresas dos EUA:', empresas_EUA)
-# print('Empresas dos CHINA:', empresas_CHINA)
-# plt.bar(['EUA', 'CHINA'], [empresas_EUA, empresas_CHINA], color=['blue', 'red'])
-# plt.xlabel('País')
-# plt.ylabel('Número de empresas')
-# plt.title('Empresas por país')
-# plt.savefig('empresas_por_pais.png')
-# plt.show()
+print('Empresas dos EUA:', empresas_EUA)
+print('Empresas dos CHINA:', empresas_CHINA)
+plt.bar(['EUA', 'CHINA'], [empresas_EUA, empresas_CHINA], color=['blue', 'red'])
+plt.xlabel('País')
+plt.ylabel('Número de empresas')
+plt.title('Empresas por país')
+plt.savefig('empresas_por_pais.png')
+plt.show()
 
 # 2
 dfPaises = pd.read_csv('data/paises.csv', delimiter=';')
 
-northern_america_countries = dfPaises[dfPaises['Region'] == 'Northern America']
+dfPaises['Region'] = dfPaises['Region'].str.strip()
+
+northern_america_countries = dfPaises[dfPaises['Region'] == 'NORTHERN AMERICA']
 
 plt.plot(northern_america_countries['Country'], northern_america_countries['Deathrate'], label='Deathrate', color='red')
 plt.plot(northern_america_countries['Country'], northern_america_countries['Birthrate'], label='Birthrate', color='blue')
