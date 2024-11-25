ebe31297f69da03114f7c673ff4dda44642652d4
Luis <luis.e@ges.inatel.br>
2024-11-24 22:22:12 -0300

    fix

---
+++ cap-4-numpy/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+np.random.seed(10)
+
+
+
+
+mtz = np.random.randint(1, 20, 9).reshape(3, 3)
+
+
+
--- ex5.py
+++ ex5.py
@@ -1,6 +1,34 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dfEmpresas = pd.read_csv('data/space.csv', delimiter=';')
 
-dfEmpresas['Country'] = dfEmpresas[]
+# 1
+# dfEmpresas = pd.read_csv('data/space.csv', delimiter=';')
+
+# dfEmpresas = dfEmpresas.drop_duplicates(subset='Company Name')
+# empresas_EUA = dfEmpresas['Location'].str.contains('USA').sum()
+# empresas_CHINA = dfEmpresas['Location'].str.contains('China').sum()
+
+# print('Empresas dos EUA:', empresas_EUA)
+# print('Empresas dos CHINA:', empresas_CHINA)
+# plt.bar(['EUA', 'CHINA'], [empresas_EUA, empresas_CHINA], color=['blue', 'red'])
+# plt.xlabel('País')
+# plt.ylabel('Número de empresas')
+# plt.title('Empresas por país')
+# plt.savefig('empresas_por_pais.png')
+# plt.show()
+
+# 2
+dfPaises = pd.read_csv('data/paises.csv', delimiter=';')
+
+northern_america_countries = dfPaises[dfPaises['Region'] == 'Northern America']
+
+plt.plot(northern_america_countries['Country'], northern_america_countries['Deathrate'], label='Deathrate', color='red')
+plt.plot(northern_america_countries['Country'], northern_america_countries['Birthrate'], label='Birthrate', color='blue')
+
+plt.xlabel('País')
+plt.ylabel('Taxa')
+plt.title('Taxa de natalidade e mortalidade por país')
+plt.legend()
+plt.savefig('taxa_natalidade_mortalidade.png')
+plt.show()
